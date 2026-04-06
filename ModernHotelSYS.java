0faeb8c56c2e5b1a83b16d52da7dc5cf9b582c2e
Stan1law <tantanmorallo213@gmail.com>
2026-04-07 00:00:07 +0800

    Update ModernHotelSYS.java

--- ModernHotelSYS.java
+++ ModernHotelSYS.java
@@ -129,7 +129,7 @@ class Hotel {
             addRoom(new Room(300 + i, "Suite", 300));
     }
 
-    // ✅ New method: checks if a room is free for given dates
+    
     public boolean isRoomAvailable(Room room, LocalDate checkIn, LocalDate checkOut) {
         for (Reservation r : reservations) {
             if (r.getRoom().getRoomNumber() == room.getRoomNumber()) {
